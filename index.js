1fb0bb17fae06d0c9a4d6524030c46b6a28aeeb5
ludmyllavaladares <ludmylla.valadares@a.ficr.edu.br>
2021-03-31 10:17:22 -0300

    add rotas

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
-console.log(app);
+// console.log(app);
+
+app.get('/', (req, res) => {
+    res.send('Olá, mundo!');
+});
+
+app.listen(3000, () =>
+    console.log('Servidor executando')
+);
